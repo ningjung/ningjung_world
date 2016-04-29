25288f51898cc880751d8a689b29e7e03bd27f4c
Allie Wilson <aswilson@andrew.cmu.edu>
2016-04-28 20:39:13 -0400

    added routes

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ActiveRecord::Base
   belongs_to :category
   
-  validate_presence_of :title, :content
+  validates_presence_of :title, :content
   
   scope :alphabetical,    -> { order('title') }
   scope :active,       -> { where(active: true) }
